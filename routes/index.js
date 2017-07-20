9d087241ce79aac9ba57c6332adcb51859e79ac1
Paul Uncangco <pju93@hotmail.com>
2017-07-19 23:19:54 -0400

    Send and Received JSON from URL

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,33 @@ router.get('/', (req, res) => {
 	// res.send('Hey! It works!');
 
 
-	const wes = {name: 'Paul', age: 100, cool: true};
+	const wes = {name: 'Wes', age: 100, cool: true};
 	/*
 	*  json(object) would display the JSON on the webpage
 	*/
 	// res.json(wes)  
 
-	//  Request 
-	res.send(req.query.name); 
-	//  Type the url like this: http://localhost:7777/?name=Paul and would print "Paul" on webpage
+	// res.json(req.query);
 
+	//  Request to send the data for the query name
+	res.send(req.query.name); 
+	//  Type the url like this: http://localhost:7777/?name=Paul and would print "Paul" on webpage 
+
+});
+
+/*	You can write like this: http://localhost:7777/reverse/paul
+*	get() is a method to receive the data from URL 
+*/
+router.get('/reverse/:name', (req, res) =>{
+	// res.send('it works!');
+	// res.send(req.params.name);
+
+	/*
+	* Reverse the word
+	* Ex: Hello = olleH
+	*/
+	const reverse = [...req.params.name].reverse().join('');
+	res.send(reverse);
 });
 
 module.exports = router;
