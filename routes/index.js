717993dcebc1d27ee274277ef3b2adbf4d68f301
Paul Uncangco <pju93@hotmail.com>
2017-07-24 23:28:35 -0400

    MVC

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
 	// console.log("Hey!!!");
 	// res.send('Hey! It works!');
 
 
-	const wes = {name: 'Wes', age: 100, cool: true};
+	// const wes = {name: 'Wes', age: 100, cool: true};
 	/*
 	*  json(object) would display the JSON on the webpage
 	*/
@@ -19,19 +19,19 @@ router.get('/', (req, res) => {
 	// res.send(req.query.name); 
 	//  Type the url like this: http://localhost:7777/?name=Paul and would print "Paul" on webpage 
 
-	res.render('hello', {
-		// Local variables 
-		// dog: 'keltian'
-		name: req.query.name,
-		dog: req.query.dog,
-		title: 'I love food' 
-	});
-});
+// 	res.render('hello', {
+// 		// Local variables 
+// 		dog: 'keltian',
+// 		name: req.query.name,
+// 		dog: req.query.dog,
+// 		title: 'I love food' 
+// 	});
+// });
 
 /*	You can write like this: http://localhost:7777/reverse/paul
 *	get() is a method to receive the data from URL 
 */
-router.get('/reverse/:name', (req, res) =>{
+// router.get('/reverse/:name', (req, res) =>{
 	// res.send('it works!');
 	// res.send(req.params.name);
 
@@ -39,8 +39,34 @@ router.get('/reverse/:name', (req, res) =>{
 	* Reverse the word
 	* Ex: Hello = olleH
 	*/
-	const reverse = [...req.params.name].reverse().join('');
-	res.send(reverse);
-});
+// 	const reverse = [...req.params.name].reverse().join('');
+// 	res.send(reverse);
+// });
+
+
+// You created a controller (/controllers/storeController.js) file which you dont need above
+
+
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController.js');
+
+router.get('/', storeController.homepage);
 
 module.exports = router;
+// console.log(router);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
