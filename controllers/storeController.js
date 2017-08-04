09cc09df078c916756ee02cbf80ac9d9b0c315fc
Paul Uncangco <pju93@hotmail.com>
2017-08-03 21:03:26 -0400

    Using Async Await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,14 +1,17 @@
-exports.homepage = (req, res) => {
-	console.log(req.name);
-	res.render('index');
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+  console.log(req.name);
+  res.render('index');
 };
 
 exports.addStore = (req, res) => {
-	// res.send('it works');
-	res.render('editStore', {title:'Add Store'});  
+  res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-	console.log(req.body);
-	res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,78 +1,10 @@
-// const express = require('express');
-// const router = express.Router();
-
-// Do work here
-// router.get('/', (req, res) => {
-	// console.log("Hey!!!");
-	// res.send('Hey! It works!');
-
-
-	// const wes = {name: 'Wes', age: 100, cool: true};
-	/*
-	*  json(object) would display the JSON on the webpage
-	*/
-	// res.json(wes)  
-
-	// res.json(req.query);
-
-	//  Request to send the data for the query name
-	// res.send(req.query.name); 
-	//  Type the url like this: http://localhost:7777/?name=Paul and would print "Paul" on webpage 
-
-// 	res.render('hello', {
-// 		// Local variables 
-// 		dog: 'keltian',
-// 		name: req.query.name,
-// 		dog: req.query.dog,
-// 		title: 'I love food' 
-// 	});
-// });
-
-/*	You can write like this: http://localhost:7777/reverse/paul
-*	get() is a method to receive the data from URL 
-*/
-// router.get('/reverse/:name', (req, res) =>{
-	// res.send('it works!');
-	// res.send(req.params.name);
-
-	/*
-	* Reverse the word
-	* Ex: Hello = olleH
-	*/
-// 	const reverse = [...req.params.name].reverse().join('');
-// 	res.send(reverse);
-// });
-
-
-// You created a controller (/controllers/storeController.js) file which you dont need above
-
-
 const express = require('express');
 const router = express.Router();
-const storeController = require('../controllers/storeController.js');
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
-// router.get('/', storeController.myMiddleware, storeController.homepage);
-
-router.get('/', storeController.homepage);
+router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-
-// post for form! 
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
-// console.log(router);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
